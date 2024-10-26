fbcbb4290967cef8ceac1608d43c515caa63f6da
Roberto Castro <robertorcastro@ua.pt>
2024-10-26 15:25:18 +0100

    TDLIE-31 first

--- backend_ToDoList/app/routes/task_routes.py
+++ backend_ToDoList/app/routes/task_routes.py
@@ -16,10 +16,17 @@ router = APIRouter(
 @router.post("/", response_model=TaskRead)
 def create_task(task_create: TaskCreate, db: Session = Depends(get_db)):
     try:
+        print(task_create)
         task_saved = task_service.create_task(db=db, task_create=task_create)
         return task_saved
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+    
+
+@router.get("/userTasks", response_model=List[TaskRead])
+def read_user_tasks(user_id: int, db: Session = Depends(get_db)):
+    tasks = task_service.get_user_tasks(db=db, user_id=user_id)
+    return tasks
     
 @router.get("/", response_model=List[TaskRead])
 def read_tasks(db: Session = Depends(get_db)):
--- backend_ToDoList/app/services/task_service.py
+++ backend_ToDoList/app/services/task_service.py
@@ -1,15 +1,26 @@
 # services/task_service.py
 from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.exc import NoResultFound
+from fastapi import HTTPException
 from app.models.task import Task
 from app.schemas.task_schema import TaskCreate
 from datetime import datetime
+from app.models.user import User
 from app.models.enums import Priority, Status
 
 def create_task(db: Session, task_create: TaskCreate) -> Task:
+    try:
+        # Check if user exists
+        user = db.query(User).filter(User.id == task_create.user_id).one()
+    except NoResultFound:
+        # Raise an HTTP exception if the user does not exist
+        raise HTTPException(status_code=404, detail="User not found")
+
     # Explicitly map the priority and status to the Enum types
     priority_enum = Priority(task_create.priority.value if isinstance(task_create.priority, Priority) else task_create.priority)
     status_enum = Status(task_create.status.value if isinstance(task_create.status, Status) else task_create.status)
-    
+
+    # Create a new Task object
     task = Task(
         title=task_create.title,
         description=task_create.description,
@@ -18,12 +29,22 @@ def create_task(db: Session, task_create: TaskCreate) -> Task:
         status=status_enum,
         created_at=datetime.now(),
         updated_at=datetime.now(),
-        user_id=task_create.user_id
+        user_id=user.id
     )
+    
     db.add(task)
     db.commit()
     db.refresh(task)
     return task
+
+def get_user_tasks(db: Session, user_id: int) -> list:
+    # Find user by id
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        # If the user does not exist, raise an HTTP exception
+        raise HTTPException(status_code=404, detail="User not found")
+
+    return db.query(Task).filter(Task.user_id == user_id).options(joinedload(Task.user)).all()
 
 def get_tasks(db: Session) -> list:
     return db.query(Task).options(joinedload(Task.user)).all()
