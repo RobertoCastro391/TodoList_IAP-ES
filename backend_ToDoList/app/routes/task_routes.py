0f1949cef31cddd68a0acff2340b6ca91f7210c0
Roberto Castro <robertorcastro@ua.pt>
2024-10-27 11:10:07 +0000

    TDLIE-32 Enpoint Implemented: Change Status of Provided Task

--- backend_ToDoList/app/routes/task_routes.py
+++ backend_ToDoList/app/routes/task_routes.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from app.models.task import Task
-from app.schemas.task_schema import TaskCreate, TaskRead
+from app.schemas.task_schema import TaskCreate, TaskRead, TaskUpdate
 from app.database import get_db
 from app.services import task_service
 
@@ -23,6 +23,14 @@ def create_task(task_create: TaskCreate, db: Session = Depends(get_db)):
 def read_user_tasks(user_id: int, db: Session = Depends(get_db)):
     tasks = task_service.get_user_tasks(db=db, user_id=user_id)
     return tasks
+
+@router.post("/updateStatus", response_model=TaskRead)
+def update_task_status(task_update: TaskUpdate, db: Session = Depends(get_db)):
+    task = task_service.update_task_status(db=db, task_update=task_update)
+
+    print("Cheguei AQUIII")
+
+    return task
     
 @router.get("/", response_model=List[TaskRead])
 def read_tasks(db: Session = Depends(get_db)):
