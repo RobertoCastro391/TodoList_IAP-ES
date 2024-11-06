154d96679d95ba34069927510a2c46d1cd14c0da
Roberto Castro <robertorcastro@ua.pt>
2024-11-06 23:58:09 +0000

    TDLIE-40 | Update endpoint for retriving data and sort

--- backend_ToDoList/app/routes/task_routes.py
+++ backend_ToDoList/app/routes/task_routes.py
@@ -1,7 +1,6 @@
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends, Query
 from sqlalchemy.orm import Session
-from typing import List
-
+from typing import List, Optional
 from app.models.task import Task
 from app.schemas.task_schema import TaskCreate, TaskRead, TaskUpdate
 from app.database import get_db
@@ -24,12 +23,30 @@ def create_task(task_create: TaskCreate, db: Session = Depends(get_db), user = D
     
 
 @router.get("/userTasks", response_model=List[TaskRead])
-def read_user_tasks(db: Session = Depends(get_db), user = Depends(auth_service.get_current_user)):
-    
+def read_user_tasks(
+    db: Session = Depends(get_db),
+    user=Depends(auth_service.get_current_user),
+    page: int = Query(1, ge=1, description="Page number starting from 1"),
+    limit: int = Query(5, ge=1, le=100, description="Number of tasks per page (max 100)"),
+    sort_by: Optional[str] = Query("creation_date", description="Field to sort by: creation_date, deadline, or completion_status"),
+    order: Optional[str] = Query("asc", description="Sort order: asc or desc"),
+    status: Optional[bool] = Query(None, description="Filter by completion status: true for completed, false for not completed")
+):
     if not user:
         raise HTTPException(status_code=401, detail="Unauthorized")
     
-    tasks = task_service.get_user_tasks(db=db, user_id=user.id)
+    skip = (page - 1) * limit
+
+    # Call the service function with sorting, filtering, and pagination
+    tasks = task_service.get_user_tasks(
+        db=db,
+        user_id=user.id,
+        skip=skip,
+        limit=limit,
+        sort_by=sort_by,
+        order=order,
+        status=status
+    )
     return tasks
 
 @router.put("/updateStatus", response_model=TaskRead)
